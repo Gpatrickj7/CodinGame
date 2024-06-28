1b4b69b9a6978c47f6dae630482dbc86fd1b01f9
Gpatrickj7 <gunnarpatrickj@gmail.com>
2024-06-28 09:09:33 -0500

    this puzzle focuses on simple, yet complex topics such as effective looping, array manipulation, and mathematical forumlae in programs. cool puzzle! im just creating this repo to store my versions and soltuions.

---
+++ defib/src/App.java
@@ -0,0 +1,99 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+
+
+
+public class App {
+
+    public static Scanner getSimulatedInput() {
+
+        String simulatedInput = "3,879483\n" +
+                                "43,608177\n" +
+                                "3\n" +
+                                "1;Maison de la Prevention Sante;6 rue Maguelone 340000 Montpellier;;3,87952263361082;43,6071285339217\n" +
+                                "2;Hotel de Ville;1 place Georges Freche 34267 Montpellier;;3,89652239197876;43,5987299452849\n" +
+                                "3;Zoo de Lunaret;50 avenue Agropolis 34090 Mtp;;3,87388031141133;43,6395872778854";
+
+                                System.out.println("Simulated Input:\n" + simulatedInput);  // Print the entire input
+        
+        return new Scanner(simulatedInput);
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+        //variables for data. data comes from console input to my understanding
+        Scanner in = getSimulatedInput();
+
+        String LON = in.next();
+        String LAT = in.next();
+        int N = in.nextInt();
+        
+
+        if (in.hasNextLine()) {
+            in.nextLine();
+        }
+        System.out.println("|".repeat(110));
+        for (int i = 0; i < N; i++) {
+            String DEFIB = in.nextLine();
+            String[] tokens = DEFIB.split(";");
+            String flagKey1 = "3,";
+            String flagKey2 = "43,";
+            boolean flag1 = false;
+            boolean flag2 = false;
+            System.out.println("Outer Outer Loop");
+
+            System.out.println(i + 1);
+
+            System.out.println(DEFIB);
+            
+            int count = 0;
+
+            for (int j = 0; j < tokens.length; j++) {
+                if (tokens[j].startsWith(flagKey1) || tokens[j].startsWith(flagKey2)){
+                    System.out.println("1");
+                    
+                    
+                    
+                }
+            }
+
+            
+
+            
+            
+                
+
+                
+
+            
+            
+           
+            
+
+        }
+
+        
+
+        
+        
+        
+
+        
+
+    }
+}
+       
